b4167c0520f2e11ecadb28b8db7f6d7751dbd581
Vebjorn Daniloff <veb.dan@hotmail.com>
2023-02-27 10:25:59 +0100

    Some commit

---
+++ chapter_6/square2.c
@@ -0,0 +1,17 @@
+// Prints a table of square using a for loop
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int i, n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) { 
+        printf("%10d%10d\n", i, i * 1);
+    }
+    return 0;
+}
