36e7ec8be53935716e4107134993c0159b5df07f
Vebjorn Daniloff <veb.dan@hotmail.com>
2023-03-13 10:54:49 +0100

    Some commit

---
+++ chapter_7/project_10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int numOfVowels = 0;
+    printf("Enter a sentence: ");
+    char c;
+    while((c = getchar()) != '\n') { 
+        switch (toupper(c)) { 
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                numOfVowels++;
+        }
+    }    
+
+    printf("Num of vowels: %d\n", numOfVowels);   
+}
